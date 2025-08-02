e45c671b0e9d972172c0dbd1b21f1af01ca9996d
Miroslav Cimerman <mc@doas.su>
2025-08-02 20:55:12 +0200

    gstripe: bitwise unmapped strips calculation
    Signed-off-by: Miroslav Cimerman <mc@doas.su>

--- sys/geom/stripe/g_stripe.c
+++ sys/geom/stripe/g_stripe.c
@@ -491,10 +491,16 @@ g_stripe_start_economic(struct bio *bp, u_int no, off_t offset, off_t length)
 		cbp->bio_length = MIN(stripesize, length);
 		if ((bp->bio_flags & BIO_UNMAPPED) != 0) {
 			cbp->bio_ma_offset += (uintptr_t)addr;
-			cbp->bio_ma += cbp->bio_ma_offset / PAGE_SIZE;
-			cbp->bio_ma_offset %= PAGE_SIZE;
+			/* cbp->bio_ma += cbp->bio_ma_offset / PAGE_SIZE */
+			cbp->bio_ma += cbp->bio_ma_offset >> PAGE_SHIFT;
+			/* cbp->bio_ma_offset %= PAGE_SIZE; */
+			cbp->bio_ma_offset &= (PAGE_SIZE - 1);
+			/*
+			 * cbp->bio_ma_n = round_page(cbp->bio_ma_offset +
+			 *     cbp->bio_length) / PAGE_SIZE;
+			 */
 			cbp->bio_ma_n = round_page(cbp->bio_ma_offset +
-			    cbp->bio_length) / PAGE_SIZE;
+			    cbp->bio_length) >> PAGE_SHIFT;
 		} else
 			cbp->bio_data = addr;
 
